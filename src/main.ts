f9bbc5e8ab1dee4b6d0454cf373006f5668f303b
Rojaebl <qgs0924@gmail.com>
2022-08-19 00:19:41 +0900

    :tada: initialize blockchain program index.ts

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import crypto from "crypto";
+
+interface BlockShape {
+  hash: string;
+  prevHash: string; // 이전해쉬값
+  height: number; // 블록의 위치
+  data: string;
+}
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
+  }
+}
